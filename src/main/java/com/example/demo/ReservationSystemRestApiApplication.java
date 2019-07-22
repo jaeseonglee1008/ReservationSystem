054d1152c64413575601c90e3f905c7fc7bdf371
Jason Lee <iamjason@bu.edu>
2019-07-22 15:13:12 -0400

    First commit

---
+++ src/main/java/com/example/demo/ReservationSystemRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationSystemRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationSystemRestApiApplication.class, args);
+	}
+
+}
